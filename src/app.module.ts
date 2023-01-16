7fb993df877c13da0ba816f2283b8b1a48b7d70f
ChoiWonYu <phillip07290@naver.com>
2023-01-17 01:56:56 +0900

    feat: users service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { ConfigModule } from '@nestjs/config';
+import { UsersService } from './users/users.service';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), EmailModule],
   controllers: [AppController, UsersController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,27 +1,36 @@
-import { Controller, Get, Param, Post, Body, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UserLoginDTO } from './dto/user-login.dto';
 import { VerifyEmailDTO } from './dto/verify-email.dto';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id')
-  getUserById(@Param('id') userId: string) {
-    console.log(userId);
-  }
+  constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  signUp(@Body() createUserDTO: CreateUserDTO) {
-    console.log(createUserDTO);
+  async create(@Body() dto: CreateUserDTO): Promise<void> {
+    const { name, email, password } = dto;
+    await this.usersService.createUser(name, email, password);
   }
 
   @Post('/email-verify')
-  verifyEmail(@Query() verifyEmailDTO: VerifyEmailDTO) {
-    console.log(verifyEmailDTO);
+  //이메일 인증
+  async verifyEmail(@Query() dto: VerifyEmailDTO) {
+    const { signupVerifyToken } = dto;
+    await this.usersService.verifyEmail(signupVerifyToken);
   }
 
   @Post('/login')
-  login(@Body() userLoginDTO: UserLoginDTO) {
-    console.log(userLoginDTO);
+  //로그인
+  async login(@Body() dto: UserLoginDTO) {
+    const { email, password } = dto;
+    await this.usersService.login(email, password);
+  }
+
+  @Get('/:id')
+  //회원 조회
+  async getUserInfo(@Param('id') id: string): Promise<string> {
+    return await this.usersService.getUserInfo(id);
   }
 }
