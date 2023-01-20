175b7648aa586deb53d1d28856a55a20397e11ee
ChoiWonYu <phillip07290@naver.com>
2023-01-20 18:40:06 +0900

    feat: validation pipe

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ValidationPipe } from './validation.pipe';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/:id')
+  getId(@Param('id', ValidationPipe) id: number) {
+    return id;
   }
 }
